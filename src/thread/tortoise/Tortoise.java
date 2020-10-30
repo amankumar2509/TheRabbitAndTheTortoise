6ac415ad2680d14b58c541c6f8d79bc86d145067
Aman Kumar <aman.kumar1_cs19@gla.ac.in>
2020-10-30 08:53:31 +0530

    add meethod tortise and add constructor for it

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,6 +2,10 @@ package thread.tortoise;
 
 public class Tortoise implements Runnable{
     private static  final int MILESTONES = 5;
+    private final Thread tortise;
+    public Tortoise(){
+        this.tortise=new Thread(this,"Tortise");
+    }
     @Override
     public void run() {
 
